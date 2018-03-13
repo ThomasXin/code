7a74adceeed897950ff539bf03be918810cb3e2d
xieyun <xhlllcl1314@sina.com>
2018-03-13 19:47:14 +0800

    获取免费代理ip

--- 2018031300/proxy.py
+++ 2018031300/proxy.py
@@ -11,13 +11,21 @@ class GainProxies():
         self.url = 'http://www.xicidaili.com/nn/'
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'}
     def get_ip_list(self):
+        # 发送请求
         web_data = requests.get(self.url, headers=self.headers)
-        soup = BeautifulSoup(web_data.text, 'lxml')
-
+        # 获取源码
+        html = web_data.text
+        # 用‘lxml’解析
+        soup = BeautifulSoup(html, 'lxml')
+        # 找出所有的‘tr’，返回的是一个列表
         ips = soup.find_all('tr')
+        # 创建数组
         ip_list = []
         for i in range(1, len(ips)):
+            # 将ips里面的每一项分别赋值给ip_info
             ip_info = ips[i]
+            # 找出每一项里面的所有td
             tds = ip_info.find_all('td')
+            # 将我们需要的每一项取出来，
             ip_list.append(tds[5].text + '://' + tds[1].text + ':' + tds[2].text)
         return ip_list
