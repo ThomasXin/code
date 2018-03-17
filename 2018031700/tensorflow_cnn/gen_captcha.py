36ce5b9e143883ddbebfdabfd14950704e3eb29e
xieyun <xhlllcl1314@sina.com>
2018-03-18 00:11:54 +0800

    修改了一些参数

--- 2018031700/tensorflow_cnn/gen_captcha.py
+++ 2018031700/tensorflow_cnn/gen_captcha.py
@@ -18,6 +18,7 @@ def random_captcha_text(char_set=number+alphabet+ALPHABET, captcha_size=4):
 	for i in range(captcha_size):
 		c = random.choice(char_set)
 		captcha_text.append(c)
+
 	return captcha_text
 
 # 生成字符对应的验证码
@@ -28,8 +29,7 @@ def gen_captcha_text_and_image():
 	captcha_text = ''.join(captcha_text)
 
 	captcha = image.generate(captcha_text)
-	#image.write(captcha_text, captcha_text + '.jpg')  # 写到文件
-
+	# image.write(captcha_text, captcha_text + '.jpg')  # 写到文件
 	#rm  =  'rm '+captcha_text + '.jpg'
 	#print rm
 	#os.system(rm)
@@ -43,12 +43,12 @@ if __name__ == '__main__':
 	# 测试
     while(1):
     	text, image = gen_captcha_text_and_image()
-    	print 'begin ',time.ctime(),type(image)
+    	# print 'begin ',time.ctime(),type(image)
     	f = plt.figure()
     	ax = f.add_subplot(111)
     	ax.text(0.1, 0.9,text, ha='center', va='center', transform=ax.transAxes)
     	plt.imshow(image)
 
 
-    	plt.show()
-    	print 'end ',time.ctime()
+    	# plt.show()
+    	# print 'end ',time.ctime()
